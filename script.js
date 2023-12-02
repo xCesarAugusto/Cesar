d4cec33e286bca25620038d16602d322a7d7ed4e
xCesarAugusto <ccesar_Augusto11@hotmail.com.com>
2023-12-02 17:00:53 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,22 @@
+function togglemode() { 
+const html = document.documentElement
+html.classList.toggle("ligth")
+
+const img =document.querySelector("#profileimg")
+
+if(html.classList.contains ('ligth')) {
+
+img.setAttribute('src', './Profile-ligth.png')
+
+} else {
+
+img.setAttribute('src',  './Profile.jpeg')
+}
+
+
+
+
+
+
+}
+
